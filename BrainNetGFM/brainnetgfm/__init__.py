d71b3afdb1145f74e1ae0b2e46f3616f7a5f75b1
BrainLabA <70477521+BrainLabA@users.noreply.github.com>
2026-05-29 14:47:16 +0800

    Delete BrainNetGFM directory

--- BrainNetGFM/brainnetgfm/__init__.py
+++
@@ -1,5 +0,0 @@
-from .configs import FinetuneConfig, PretrainConfig
-from .layers import GraphBackbone, MultiTaskGNN
-from .ssl_model import GraphSSLModel
-
-__all__ = ['PretrainConfig', 'FinetuneConfig', 'GraphSSLModel', 'GraphBackbone', 'MultiTaskGNN']
